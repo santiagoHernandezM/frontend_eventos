d6b20767e2ecfa555d336e2a05df0759860cfdcc
VanessaLopezLU <santiagohernandez.2005@outlook.es>
2024-04-09 15:55:04 -0500

    Cargue masivo usuario funcionando con contratos opcionales

--- src/views/Usuario/MasivoUsuarioView.vue
+++ src/views/Usuario/MasivoUsuarioView.vue
@@ -39,87 +39,69 @@
       </v-card>
     </v-row>
 
-    <!-- Listado -->
+    <!-- TABLA -->
+    <!-- <Tabla
+      :items="usuarios"
+      :cabecera="cabeceraTabla"
+      style="border: 1px solid red;"
+    /> -->
     <template>
-      <v-row justify="space-around">
-        <v-card width="900" class="mt-12 px-10 py-10">
-          <div>
-            <div class="text-center d-flex pb-4">
-              <v-btn @click="all"> all </v-btn>
-              <!--
-              <div>{{ panel }}</div>
-              -->
-              <v-btn @click="none"> none </v-btn>
-            </div>
+    <v-row justify="space-around">
+      <v-card max-width="900" class="mt-12 px-10 py-10">
+        <v-data-table
+          :items="usuarios"
+          :headers="cabeceraTabla"
+          class="elevation-1"
+          :search="search"
+          :custom-filter="filterOnlyCapsText"
+        >
+          <!-- Buscador -->
+          <template v-slot:top>
+            <v-text-field
+              v-model="search"
+              append-icon="mdi-magnify"
+              label="Buscar"
+              @input="convertToUpperCase"
+              class="mx-4"
+              color="rgb(52,188,52)"
+            ></v-text-field>
+          </template>
 
-            <v-expansion-panels v-model="panel" multiple>
-              <!-- v-for="(usuario, i) in usuarios" :key="i" -->
-              <v-expansion-panel>
-                <v-expansion-panel-header>Archivo 1</v-expansion-panel-header>
+          <!-- Listado -->
+          <template v-slot:item.actions="{ item }">
+            <v-card-actions style="padding: 0">
+              <v-btn
+                icon
+                @click="metodoEditar(item)"
+                @mouseover="mostrarTooltip = true"
+                @mouseleave="mostrarTooltip = false"
+                id="btn-editar"
+              >
+                <v-icon id="icono-editar">mdi-pencil</v-icon>
+              </v-btn>
 
-                <v-expansion-panel-content>
-                  <!-- Tabla -->
-                  <!-- <Tabla :items="usuarios" :cabecera="cabeceraTabla" /> -->
-                  <v-data-table
-                    :items="usuarios"
-                    :headers="cabeceraTabla"
-                    :search="search"
-                    :custom-filter="filterOnlyCapsText"
-                    style="margin: 0"
-                  >
-                    <!-- Buscador -->
-                    <template v-slot:top>
-                      <v-text-field
-                        v-model="search"
-                        append-icon="mdi-magnify"
-                        label="Buscar"
-                        @input="convertToUpperCase"
-                        class="mx-4"
-                        color="rgb(52,188,52)"
-                      ></v-text-field>
-                    </template>
-
-                    <!-- Listado -->
-                    <template v-slot:item.actions="{ item }">
-                      <v-card-actions style="padding: 0">
-                        <v-btn
-                          icon
-                          @click="metodoEditar(item)"
-                          @mouseover="mostrarTooltip = true"
-                          @mouseleave="mostrarTooltip = false"
-                          id="btn-editar"
-                        >
-                          <v-icon id="icono-editar">mdi-pencil</v-icon>
-                        </v-btn>
-
-                        <v-btn
-                          icon
-                          @click="metodoEliminar(item)"
-                          id="btn-eliminar"
-                        >
-                          <v-icon id="icono-eliminar">mdi-delete</v-icon>
-                        </v-btn>
-                      </v-card-actions>
-                    </template>
-                  </v-data-table>
-                </v-expansion-panel-content>
-              </v-expansion-panel>
-            </v-expansion-panels>
-          </div>
-        </v-card>
-      </v-row>
-    </template>
+              <v-btn icon @click="metodoEliminar(item)" id="btn-eliminar">
+                <v-icon id="icono-eliminar">mdi-delete</v-icon>
+              </v-btn>
+            </v-card-actions>
+          </template>
+        </v-data-table>
+      </v-card>
+    </v-row>
+  </template>
 
     <!-- Cargando... -->
-    <Spinner :value="loading" />
+    <!-- <Spinner :value="loading" /> -->
 
     <pre>{{ $data }}</pre>
   </v-container>
 </template>
 <script>
 import axios from "axios";
+// import Tabla from "../../components/Tabla.vue"
 
 export default {
+  // components: { Tabla },
   data() {
     return {
       api: `${process.env.VUE_APP_API_URL}:${process.env.VUE_APP_API_PORT}`,
@@ -140,7 +122,7 @@ export default {
         { text: "Nombres", value: "nombre" },
         { text: "Apellidos", value: "apellido" },
         { text: "Teléfono", value: "celular" },
-        { text: "Acciones", value: "actions" },
+        { text: "Registrado", value: "registrado" },
       ],
 
       search: "",
@@ -156,19 +138,28 @@ export default {
         formData.append("file", this.file);
         formData.append("centro", centro);
 
-        await axios
-          .post(
-            `${this.api}/carguemasivocompetencias/cargarinstructor/`,
-            formData
-          )
-          .then(function (response) {
-            console.log(response);
-          })
-          .catch(function (error) {
-            {
-              console.log(error);
-            }
-          });
+        try {
+          const response = await axios.post(`${this.api}/carguemasivocompetencias/cargarinstructor/`, formData)
+          console.log(response)
+          this.usuarios = response.data
+
+        } catch (error) {
+          console.log(error)
+        }
+
+        // await axios
+        //   .post(
+        //     `${this.api}/carguemasivocompetencias/c  argarinstructor/`,
+        //     formData
+        //   )
+        //   .then(function (response) {
+        //     console.log(response);
+        //   })
+        //   .catch(function (error) {
+        //     {
+        //       console.log(error);
+        //     }
+        //   });
       }
       this.loading = false;
     },
@@ -205,8 +196,8 @@ export default {
     const programasResponse = await axios.get(`${this.api}/programas/`);
     this.programas = programasResponse.data;
 
-    const usuariosResponse = await axios.get(`${this.api}/users/instructores`);
-    this.usuarios = usuariosResponse.data;
+    // const usuariosResponse = await axios.get(`${this.api}/users/instructores`);
+    // this.usuarios = usuariosResponse.data;
   },
 };
 </script>
